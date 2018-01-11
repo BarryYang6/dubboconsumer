477e9b255309e87fd22bcdbdc21a04f459838d0a
Barry <yang_rui2016@163.com>
2018-01-11 22:32:39 +0800

    first commit

---
+++ src/main/java/com/hnair/demo/Consumer.java
@@ -0,0 +1,15 @@
+package com.hnair.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class Consumer {
+    public static void main (String[] args) throws IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring/sample-consumer.xml"});
+        context.start();
+        System.out.println("服务订阅成功");
+        DubboProvider dubboProvider = (DubboProvider) context.getBean("dubboProvider");
+        System.out.println(dubboProvider.sayHello("Barry"));
+    }
+}
